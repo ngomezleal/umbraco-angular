88ab5f19cb1e6710120820b86f169eed7c0c6504
Nelson Gomez <ngomezleal@gmail.com>
2024-06-13 12:23:34 -0500

    Demo umbraco with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'umbraco';
+  result: string;
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngOnInit() {
+    this.result = this.elementRef.nativeElement.getAttribute("avianca-values");
+  }
 }
